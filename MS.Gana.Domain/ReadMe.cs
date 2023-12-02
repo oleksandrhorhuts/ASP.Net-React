244bbb86ad0dcafb38eedf259d6d427aa470c17f
oleksandrhorhuts <oleksandrhorhuts@gmail.com>
2023-12-02 10:51:14 +0100

    read me

--- MS.Gana.Domain/ReadMe.cs
+++ MS.Gana.Domain/ReadMe.cs
@@ -2,7 +2,8 @@
 {
     public class ReadMe
     {
-        //All business logic will happen in this layer.
-        //For now, there isn't much business logic except maybe 
+        //All business logic will happen in this layer and all domain related stuff.
+        //For now, there isn't much business logic except maybe filtering the data using Trie structure.
+        //also all interfaces and POCO models should live here
     }
 }
